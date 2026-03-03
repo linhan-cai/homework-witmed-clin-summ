21b48816d312d5f9d43f1aa99eb5d19483b774f0
linhancai <linhan.cai@qq.com>
2026-03-03 16:03:04 +0800

    .

--- src/0_fineture_dataset.py
+++ src/0_fineture_dataset.py
@@ -1,5 +1,5 @@
-import requests
 import json 
+import os
 
 from dotenv import dotenv_values
 config = dotenv_values(".env")
@@ -7,19 +7,33 @@ config = dotenv_values(".env")
 
 def main():
     
+    output_path = "./dataset/fineture"
     files = [
-        ("./dataset/chq.jsonl", "./dataset/fineture/chq.json"), 
-        ("./dataset/d2n.jsonl", "./dataset/fineture/d2n.json"),
-        ("./dataset/opi.jsonl", "./dataset/fineture/opi.json"),
+        ("./dataset/chq.jsonl", 100, 200), 
+        ("./dataset/d2n.jsonl", 20, 30),
+        ("./dataset/opi.jsonl", 100, 200),
     ]
 
-    for source_file, target_file in files:
-        with open(source_file, "r") as fd_r, open(target_file, "a+") as fd_w:
-            for row in fd_r:
+    dev_file = os.path.join(output_path, "dev.json")
+    train_file = os.path.join(output_path, "train.json")
+    for source_file, test_i, dev_i in files:
+        with open(source_file, "r") as fd_r, open(dev_file, "a+") as fd_dev, open(train_file, "a+") as fd_train:
+            for i, row in enumerate(fd_r):
+                if i < test_i:
+                    continue
+
                 dct = json.loads(row)
-                sample = {'conversations': [{'role': 'user', 'content': dct['inputs']},
-                                            {'role': 'assistant', 'content': dct['target']}]}
-                fd_w.write(json.dumps(sample, ensure_ascii=False) + "\n")
+                sample = {
+                    'conversations': [
+                        {'role': 'user', 'content': dct['inputs']},
+                        {'role': 'assistant', 'content': dct['target']}
+                    ]
+                }
+                
+                if i < dev_i:
+                    fd_dev.write(json.dumps(sample, ensure_ascii=False) + "\n")
+                else:
+                    fd_train.write(json.dumps(sample, ensure_ascii=False) + "\n")
 
 if __name__ == "__main__":
     main()
